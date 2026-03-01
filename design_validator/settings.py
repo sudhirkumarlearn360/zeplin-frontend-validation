31dd39a9d7f9aede1fd1d7bb03550cb55f27ba1c
Sudhir Kumar <sudhir.kumar@careers360.com>
2026-03-01 10:17:46 +0530

    feat: Configure database switcher for mysql/sqlite based on env vars

--- design_validator/settings.py
+++ design_validator/settings.py
@@ -86,12 +86,26 @@ WSGI_APPLICATION = 'design_validator.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+DB_TYPE = os.environ.get('DB_TYPE', 'sqlite').lower()
+
+if DB_TYPE == 'mysql':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.environ.get('DB_NAME', 'zeplin_validator'),
+            'USER': os.environ.get('DB_USER', 'root'),
+            'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+            'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
+            'PORT': os.environ.get('DB_PORT', '3306'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
